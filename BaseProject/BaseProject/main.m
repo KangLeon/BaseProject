9f911b2c9375d6305124a2ea7e90f1e48a461bed
jitengjiao <tengjiao.ji@dragonsoftbravo.com>
2018-11-23 15:01:42 +0800

    new
    new

---
+++ BaseProject/BaseProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BaseProject
+//
+//  Created by admin on 2018/11/23.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
